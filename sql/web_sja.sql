3ccde54dace876e368e16e8402b7ec35d5d4adf8
EOD <eric.defoix@elca.mu>
2019-09-26 17:54:12 +0400

    Member & Certificate Table

--- sql/web_sja.sql
+++ sql/web_sja.sql
@@ -42,7 +42,7 @@ create table sja_address (
   ADDRESS_CITY varchar(50) not null,
   ADDRESS_STATE varchar(50) not null,
   ADDRESS_COUNTRY varchar(50) not null,
-  ADDRESS_POSTAL_CODE text not null,
+  ADDRESS_POSTAL_CODE text null,
   constraint PK_ADDRESS_ID primary key (ADDRESS_ID),
   constraint AK_ADDRESS unique key (ADDRESS_HOUSE_NO, ADDRESS_STREET, ADDRESS_CITY, ADDRESS_STATE, ADDRESS_COUNTRY)
 );
@@ -54,10 +54,40 @@ create table sja_division (
 	constraint FK_DIVISION_AREA foreign key (AREA_NAME) references sja_area(AREA_NAME)
 ); 
 
+create table sja_member (
+	MEMBER_NIC varchar(14) not null,
+	MEMBER_FIRST_NAME text not null,
+	MEMBER_MIDDLE_NAME text null,
+	MEMBER_MAIDEN_NAME text null,
+	MEMBER_LAST_NAME text not null,
+	MEMBER_DOB date not null,
+	MEMBER_EMAIL text null,
+	MEMBER_HOME_PHONE text null,
+	MEMBER_MOBILE_PHONE text null,
+	MEMBER_ADDRESS int not null,
+	MEMBER_GENDER varchar(11) not null,
+	MEMBER_DATE_JOINED date not null,
+	MEMBER_IMAGE longblob null,
+	constraint PK_MEMBER_ID primary key (MEMBER_NIC),
+	constraint FK_MEMBER_ADDRESS_ADDRESS_ID foreign key (MEMBER_ADDRESS) references sja_address(ADDRESS_ID),
+	constraint FK_MEMBER_GENDER_GENDER_NAME foreign key (MEMBER_GENDER) references sja_gender(GENDER_NAME)
+);
+
+create table sja_certificate (
+	CERTIFICATE_ID varchar(20) not null,
+	MEMBER_ID varchar(14) not null,
+	CERTIFICATE_DATE_ISSUE date not null,
+	CERTIFICATE_DATE_EXPIRY date not null,
+	CERTIFICATE_LEVEL int not null,
+	constraint PK_CERTIFICATE_ID primary key (CERTIFICATE_ID),
+	constraint FK_CERTIFICATE_MEMBER_ID foreign key (MEMBER_ID) references sja_member(MEMBER_NIC),
+	constraint FX_CERTIFICATE_FIRST_AID_LEVEL foreign key (CERTIFICATE_LEVEL) references sja_fa_level(FA_NAME)
+);
+
 /*
-# Member Table
-# First Aiders Table
+# Member Table *
+# First Aiders Table 
 # Duty Table
 # Joined Duty && Member Table
-# Certificate Table
+# Certificate Table *
 # User Admin Table
